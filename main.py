89ff3aff8e1ebc1b13d6273ce513f38f517519f3
wiktor1979 <wiktorkmieciak@gmail.com>
2026-08-12 23:36:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -124,7 +124,9 @@ def main():
     try:
         while True:
             time.sleep(1)
+             logging.info("sleep")
     except KeyboardInterrupt:
+         logging.info("koniec")
         open_pulsar.stop()
 
 
