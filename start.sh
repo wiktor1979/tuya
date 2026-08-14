8556bfa88efecf40c5cee555c544813b0a88a1fe
wiktor1979 <wiktorkmieciak@gmail.com>
2026-08-14 21:37:15 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-# 1. Uruchomienie logera danych w tle
+# 1. Uruchomienie zbierania danych w tle
 python main.py &
 
-# 2. Uruchomienie Streamlita na porcie 8501 (przechodzi na pierwszy plan)
-exec streamlit run dashboard.py --server.port=8501 --server.address=0.0.0.0 --server.headless=true
+# 2. Glowny proces (Streamlit) na pierwszym planie
+exec python -m streamlit run dashboard.py --server.port 8501 --server.address 0.0.0.0
