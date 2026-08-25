4ce763c771ad30ed7f28c469cceeea826ea7f2fe
wiktor1979 <wiktorkmieciak@gmail.com>
2026-08-25 13:19:34 +0200

    Update config.py

--- app/config.py
+++ app/config.py
@@ -57,17 +57,17 @@ TEMP_CODES = {
 
 HISTERESIS_CONFIG = {
     # Temperatury hydrauliczne
-    "out_water_temp": {"active": 0.2, "idle": 0.5, "last_value": None},
-    "in_water_temp":  {"active": 0.2, "idle": 0.5, "last_value": None},
-    "tank_temp":      {"active": 0.3, "idle": 0.5, "last_value": None},
+    "out_water_temp": {"active": 2, "idle": 5, "last_value": None},
+    "in_water_temp":  {"active": 2, "idle": 5, "last_value": None},
+    "tank_temp":      {"active": 3, "idle": 5, "last_value": None},
 
     # Temperatury otoczenia i wewnętrzne
-    "amb_temp":       {"active": 0.5, "idle": 0.8, "last_value": None},
-    "tidr":           {"active": 0.5, "idle": 0.5, "last_value": None},  # Dodano: redukuje szum o ~97%
+    "amb_temp":       {"active": 5, "idle": 8, "last_value": None},
+    "tidr":           {"active": 5, "idle": 5, "last_value": None},  # Dodano: redukuje szum o ~97%
 
     # Temperatury układu chłodniczego
-    "disc_temp":      {"active": 0.5, "idle": 1.5, "last_value": None},  # W idle powolny dryf do temp. otoczenia
-    "back_temp":      {"active": 0.5, "idle": 1.5, "last_value": None},
+    "disc_temp":      {"active": 5, "idle": 15, "last_value": None},  # W idle powolny dryf do temp. otoczenia
+    "back_temp":      {"active": 5, "idle": 15, "last_value": None},
 
     # Energia i zasilanie
     "ac_curr":        {"active": 1.0, "idle": 1.0, "last_value": None},  # Skala x10 (2.0 = krok 0.2A)
