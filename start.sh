626ad43031974e96c3e1bc8f11c11c2da3a045ed
wiktor1979 <wiktorkmieciak@gmail.com>
2026-08-27 16:34:41 +0200

    Squashed commit of the following:
    commit 4061221616ab2c78ecbaf93db00dd022dda8e0ee
    Merge: b4f248e 25e8405
    Author: wiktor1979 <wiktorkmieciak@gmail.com>
    Date:   Thu Aug 27 16:33:16 2026 +0200
    
        Merge branch 'dev' of https://github.com/wiktor1979/tuya into dev
    
    commit b4f248e008b1a18fe11ce3cb7948dbdf87cf85d3
    Author: wiktor1979 <wiktorkmieciak@gmail.com>
    Date:   Thu Aug 27 16:33:02 2026 +0200
    
        zmiana nazwy glownego pliku
    
    commit 25e8405cbb88e930f68974653d0e9fe6ca9139dd
    Author: wiktor1979 <wiktorkmieciak@gmail.com>
    Date:   Thu Aug 27 16:19:15 2026 +0200
    
        Update styles.py

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@
 python main.py &
 
 # 2. Glowny proces (Streamlit) na pierwszym planie
-exec python -m streamlit run dashboard.py --server.port 8501 --server.address 0.0.0.0
+exec python -m streamlit run Panel.py --server.port 8501 --server.address 0.0.0.0
